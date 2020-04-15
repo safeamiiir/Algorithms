38894a5404790afb08c10b7b3b1299f1613ea69c
safeamiiir <safehian_amirreza@yahoo.com>
2020-04-15 22:01:13 +0430

    fix a problem but still not working for most equls

--- Exc_3/answer_1.py
+++ Exc_3/answer_1.py
@@ -4,12 +4,15 @@ blocks = 0
 while( True ):
     left = []
     blocks += 1
+    print('index', right.index(1))
     for index in range(right.index(min(right))+1):
         left.append(right.pop(0)) # We initialized left and right arrays
+    print('left: ', left)
+    print('right: ', right)
     if( len(right) > 0 ):
         if(max(left) > min(right) ):
             for index in range(right.index(min(right))+1):
-                left.append(right.pop(index))
+                left.append(right.pop(0))
     else:
         break
 print(blocks)
