43cf39e1aaacc66fecd154d65094fc33278c762e
safeamiiir <safehian_amirreza@yahoo.com>
2020-04-16 00:36:53 +0430

    fix repeated numbers

--- Exc_3/answer_1.py
+++ Exc_3/answer_1.py
@@ -4,14 +4,11 @@ blocks = 0
 while( True ):
     left = []
     blocks += 1
-    print('index', right.index(1))
-    for index in range(right.index(min(right))+1):
+    for index in range([index for index, value in enumerate(right) if value == min(right)][-1]+1):
         left.append(right.pop(0)) # We initialized left and right arrays
-    print('left: ', left)
-    print('right: ', right)
     if( len(right) > 0 ):
         if(max(left) > min(right) ):
-            for index in range(right.index(min(right))+1):
+            for index in range([index for index, value in enumerate(right) if value == min(right)][-1]+1):
                 left.append(right.pop(0))
     else:
         break
