4f26d40d089446b234a40c7b4373790e01cd8022
safeamiiir <safehian_amirreza@yahoo.com>
2020-04-15 21:41:16 +0430

    Exc_3 Q_1 Done

--- Exc_3/answer_1.py
+++ Exc_3/answer_1.py
@@ -1,13 +1,15 @@
 number = input("Please enter number of towers: ")
 right = [int(tower) for tower in input("Please enter height of towers: ").split()]
 blocks = 0
-print(right)
-while( len(right) > 0 ):
-    print('right: ', right)
+while( True ):
     left = []
     blocks += 1
-    left.append(right.pop(0))
-    if(max(left) > min(right) ):
-        for index in range(right.index(min(right))+1):
-            left.append(right.pop(index))
-print('blocks: ', blocks)
+    for index in range(right.index(min(right))+1):
+        left.append(right.pop(0)) # We initialized left and right arrays
+    if( len(right) > 0 ):
+        if(max(left) > min(right) ):
+            for index in range(right.index(min(right))+1):
+                left.append(right.pop(index))
+    else:
+        break
+print(blocks)
