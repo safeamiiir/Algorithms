072c8e4dc395128611a4fb81d1c956ecd9a0a3c1
safeamiiir <safehian_amirreza@yahoo.com>
2020-04-17 00:53:15 +0430

    fix min of []

--- Exc_3/answer_1.py
+++ Exc_3/answer_1.py
@@ -1,17 +1,14 @@
-number = input("Please enter number of towers: ")
-right = [int(tower) for tower in input("Please enter height of towers: ").split()]
+number = input()
+right = [int(tower) for tower in input().split()]
 blocks = 0
 while( len(right) > 0 ):
     left = []
     blocks += 1
     minimum = [index for index, value in enumerate(right) if value == min(right)][0]
-    for itemIndex in range(minimum+1):
+    for itemIndex in range(minimum + 1):
         left.append(right.pop(0)) # We initialized left and right arrays
-    if( len(right) > 0 ):  
-        while(max(left) > min(right) ):
-            minimum = [index for index, value in enumerate(right) if value == min(right)][0]
-            for index in range(minimum+1):
-                left.append(right.pop(0))             
-    else:
-        break
+    while(len(right) > 0 and max(left) > min(right) ):
+        minimum = [index for index, value in enumerate(right) if value == min(right)][0]
+        for index in range(minimum + 1):
+            left.append(right.pop(0))             
 print(blocks)
