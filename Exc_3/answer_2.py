7e0230d63d052f2e41ff354caf1d2d307322ba81
safeamiiir <safehian_amirreza@yahoo.com>
2020-04-17 05:33:42 +0430

    ans_2 in python done

--- Exc_3/answer_2.py
+++ Exc_3/answer_2.py
@@ -1,14 +1,31 @@
-number = input()
-sequence = [int(tower) for tower in input().split()]
-ans = []
-while(len(sequence) > 0):
-    minimum = min(sequence)
-    ans.append(minimum)
-    for i in range(sequence.index(minimum)+1):
-        sequence.pop(0)        
-    if(len(sequence) > 0):
-        maximum = max(sequence)
-        ans.append(maximum)
-        for i in range(sequence.index(maximum)+1):
-            sequence.pop(0)
-print(len(ans))
+n = int(input())
+l = list(map(int, input().split()))
+ans = 1
+if n == 1:
+    print(ans)
+else:
+    a = l[0]
+    flag = True   # find next large element
+    for i in range(1, n):
+        if l[i] > a:
+            a = l[i]
+            ans += 1
+            flag = False
+            break
+        if l[i] < a:
+            a = l[i]
+            flag = True
+            if(ans > 1):
+                ans += 1
+            break
+    for j in range(i, n):
+        if flag == True:
+            if l[j] > a:
+                ans += 1
+                flag = False
+        else:
+            if l[j] < a:
+                ans += 1
+                flag = True
+        a = l[j]
+    print(ans)
